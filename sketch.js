3fe2a76802b2b9831f0e0c9977021cc24c7ceb32
LazyJoe666 <Joeao1026@gmail.com>
2024-10-18 22:41:42 -0400

    hw06

--- sketch.js
+++ sketch.js
@@ -1,6 +1,28 @@
+let data = [];
+
+function preload() {
+  data = loadTable("Beijing-PM2.5.csv", "csv", "header");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(128);
+  stroke(40);
+  strokeWeight(0.2);
+
+
+  for (let idx = 0; idx < data.getRowCount(); idx++) {
+
+    let s = data.getNum(idx, "pm2.5");
+    let x = data.getNum(idx, "month");
+    let s1 = map(s, 10, 200, 50, 150);
+    let x1 = map(x, 1, 12, 0, windowWidth) ;
+    noFill();
+    ellipse(x1, height/2, s1);
+  }
+  
 }
+
 
 function draw() {
 }
