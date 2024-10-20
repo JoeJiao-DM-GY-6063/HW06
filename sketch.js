7f68dd9de0c58fe2a95ee3bdc0a42c268d6dc2ee
LazyJoe666 <Joeao1026@gmail.com>
2024-10-20 15:46:51 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,19 +10,14 @@ function setup() {
   stroke(40);
   strokeWeight(0.2);
 
-
   for (let idx = 0; idx < data.getRowCount(); idx++) {
-
     let s = data.getNum(idx, "pm2.5");
     let x = data.getNum(idx, "month");
     let s1 = map(s, 10, 200, 50, 150);
-    let x1 = map(x, 1, 12, 0, windowWidth) ;
+    let x1 = map(x, 1, 12, 0, windowWidth);
     noFill();
-    ellipse(x1, height/2, s1);
+    ellipse(x1, height / 2, s1);
   }
-  
 }
 
-
-function draw() {
-}
+function draw() {}
